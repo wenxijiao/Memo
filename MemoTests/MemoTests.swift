068de7e414c900d580622b2c9157ab36c780f90d
vincent <vincentjiao.dream@gmail.com>
2025-04-26 15:05:13 +1200

    Initial commit

---
+++ MemoTests/MemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  MemoTests.swift
+//  MemoTests
+//
+//  Created by Vincent Jiao on 26/04/2025.
+//
+
+import Testing
+
+struct MemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
